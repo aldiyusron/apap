c04e57dae4c165cfa9d22afc5ffac8be5fa32d01
annisa <annisa@192.168.1.100>
2017-03-13 14:54:00 +0700

    inisiasi beberapa model

---
+++ src/main/java/mosing/MosingApplication.java
@@ -0,0 +1,12 @@
+package mosing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosingApplication.class, args);
+	}
+}
---
+++ src/main/java/mosing/model/PemimpinUniv.java
@@ -0,0 +1,5 @@
+package mosing.model;
+
+public class PemimpinUniv {
+
+}
