9ae179ea8cf85e094a4136e20d949b1d99ed72fe
Muhammad Aldi Yusron <mhmmduso@gmail.com>
2017-04-18 18:26:38 +0700

    lokasi mapper, model, service, serviceDatabase

--- src/main/java/mosing/mapper/LokasiMapper.java
+++ src/main/java/mosing/mapper/LokasiMapper.java
@@ -2,33 +2,47 @@ package mosing.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import mosing.model.LokasiModel;
 
 @Mapper
 public interface LokasiMapper {
 
-	@Select("select * from lokasi where nama_lokasi = #{nama_lokasi}")
+	@Select("select * from lokasi where id_lokasi = #{id_lokasi}")
 	@Results(value = { 
 	    		@Result(property = "alamat", column = "alamat"), 
 	    		@Result(property = "no_telp", column = "no_telp"),
 				@Result(property = "nama_lokasi", column = "nama_lokasi"),
-				@Result(property = "provinsi", column = "provinsi"),
-				@Result(property = "kota", column = "kota")})
-	LokasiModel selectLokasi(@Param("nama_lokasi") String nama_lokasi);
+				@Result(property = "id_provinsi", column = "id_provinsi"),
+				@Result(property = "id_kota", column = "id_kota"),
+				@Result(property = "kuota_peng", column = "kuota_peng"),
+				@Result(property = "kuota_pendaftar", column = "kuota_pendaftar")})
+	LokasiModel selectLokasi(@Param("id_lokasi") int id_lokasi);
 	
 	@Select("select alamat, no_telp, nama_lokasi, provinsi, kota from lokasi")
-
 	@Results(value = { 
     		@Result(property = "alamat", column = "alamat"), 
     		@Result(property = "no_telp", column = "no_telp"),
 			@Result(property = "nama_lokasi", column = "nama_lokasi"),
-			@Result(property = "provinsi", column = "provinsi"),
-			@Result(property = "kota", column = "kota")})
+			@Result(property = "id_provinsi", column = "id_provinsi"),
+			@Result(property = "id_kota", column = "id_kota"),
+			@Result(property = "kuota_peng", column = "kuota_peng"),
+			@Result(property = "kuota_pendaftar", column = "kuota_pendaftar")})
 	List<LokasiModel> selectAllLokasi();
+
+	@Insert("insert into lokasi (alamat, no_telp, nama_lokasi, id_provinsi, id_kota, kuota_peng, kuota_pendaftar) values (#{alamat}, #{no_telp}, #{nama_lokasi}, #{id_provinsi}, #{id_kota}, #{kuota_peng}, #{kuota_pendaftar})")
+	void addLokasiUjian(LokasiModel lokasi);
+
+	@Update("update lokasi set alamat = #{alamat}, no_telp = #{no_telp}, nama_lokasi = #{nama_lokasi}, id_provinsi = #{id_provinsi}, id_kota = #{id_kota}, kuota_peng = #{kuota_peng}, kuota_pendaftar = #{kuota_pendaftar} where id_lokasi = #{id_lokasi}")
+	void updateLokasiUjian(LokasiModel lokasi);
+
+	@Update("update lokasi set flag = 0 where id_lokasi = #{id_lokasi}}")
+	void deleteLokasiUjian(LokasiModel lokasi);
 }
--- src/main/java/mosing/service/LokasiService.java
+++ src/main/java/mosing/service/LokasiService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import mosing.model.LokasiModel;
 
 public interface LokasiService {
-
-	LokasiModel selectLokasi(String nama_lokasi);
+	void addLokasiUjian (LokasiModel lokasi);
+	void updateLokasiUjian (LokasiModel lokasi);
+	void deleteLokasiUjian (LokasiModel lokasi);
 	List<LokasiModel> selectAllLokasi();
+	LokasiModel selectLokasi(int id_lokasi);
 }
